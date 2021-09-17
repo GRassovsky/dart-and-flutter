fac20b465d183df16ab77efc6a3ceedd837a51f3
George Rassovsky <grassovsky@Georges-iMac.broadband>
2021-09-17 10:02:11 +0100

    Adding a Username and Password fields to our login screen app, which accept input

---
+++ udemy-dart-and-flutter/login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget
+{
+    createState()
+    {
+        return LoginScreenState();
+    }
+}
+
+
+class LoginScreenState extends State<LoginScreen>
+{
+    Widget build(context)
+    {
+        return Container(
+            margin: EdgeInsets.all(20.0),
+            child: Form(
+                child: Column(
+                    children: [
+                        emailField(),
+                        passwordField(),
+                        //submitButton(),
+                    ],
+                ),
+            ),
+        );
+    }
+
+    Widget emailField()
+    {
+        return TextFormField(
+            // We don't need to create an instance of the class
+            // because we are referencing a constant in the class
+            keyboardType: TextInputType.emailAddress, 
+            
+            decoration: InputDecoration(
+                labelText: 'Email Address:',
+                hintText: 'you@example.com',
+            ),
+            
+        );
+    }
+
+    Widget passwordField()
+    {
+        return TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+                labelText: 'Password:',
+                hintText: 'password',
+            ),
+        );
+    }
+
+    // Widget submitButton()
+    // {
+
+    // }
+}
